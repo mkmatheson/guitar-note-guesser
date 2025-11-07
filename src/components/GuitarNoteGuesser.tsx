6ea8ced4e0cc696d5f7155eac9c9159df19d9510
mkmatheson <mckaymatheson@gmail.com>
2025-11-06 22:27:34 -0800

    remove nits from guesser

--- src/components/GuitarNoteGuesser.tsx
+++ src/components/GuitarNoteGuesser.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import React, { useState, useEffect, useRef } from 'react';
 import { generateNote } from '../utils';
 import { standardTuning, completeFretboard } from '../const';
@@ -8,7 +7,7 @@ const totalNotes = completeFretboard.flatMap((string) =>
 ).length;
 
 const GuitarNoteGuesser = () => {
-    const [data, setData] = useState(generateNote());
+  const [data, setData] = useState(generateNote());
   const [showAnswer, setShowAnswer] = useState(false);
   const [notes, setNotes] = useState(completeFretboard);
   const showButtonRef = useRef<HTMLButtonElement>(null);
@@ -33,80 +32,82 @@ const GuitarNoteGuesser = () => {
       window.removeEventListener('keydown', handleKeyPress);
     };
   }, []); // Empty dependency array ensures this runs once on mount and unmount
-    return(<>
-        <div>string {data.string}</div>
-        <div>fret {data.fret}</div>
-        <div>
-          <button ref={showButtonRef} onClick={() => setShowAnswer(true)}>
-            Show answer
-          </button>
-          <button
-            ref={resetButtonRef}
-            onClick={() => {
-              const oldNotes = notes;
-              if (showAnswer) {
-                oldNotes[data.string - 1][data.fret] = true;
-                setNotes(oldNotes);
-                setShowAnswer(false);
+  return (
+    <>
+      <div>string {data.string}</div>
+      <div>fret {data.fret}</div>
+      <div>
+        <button ref={showButtonRef} onClick={() => setShowAnswer(true)}>
+          Show answer
+        </button>
+        <button
+          ref={resetButtonRef}
+          onClick={() => {
+            const oldNotes = notes;
+            if (showAnswer) {
+              oldNotes[data.string - 1][data.fret] = true;
+              setNotes(oldNotes);
+              setShowAnswer(false);
+            }
+            let note = undefined;
+            while (!note) {
+              const newNote = generateNote();
+              if (!oldNotes[newNote.string - 1][newNote.fret]) {
+                note = newNote;
               }
-              let note = undefined;
-              while (!note) {
-                const newNote = generateNote();
-                if (!oldNotes[newNote.string - 1][newNote.fret]) {
-                  note = newNote;
-                }
-              }
-              setData(note);
-            }}
-          >
-            Reset
-          </button>
-        </div>
-        <div>
-          Notes remaining:{' '}
-          {notes.flatMap((string) => string.filter((note) => !note)).length} /{' '}
-          {totalNotes}
-        </div>
+            }
+            setData(note);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+      <div>
+        Notes remaining:{' '}
+        {notes.flatMap((string) => string.filter((note) => !note)).length} /{' '}
+        {totalNotes}
+      </div>
 
-        <div style={{ minHeight: '250px' }}>
-          {showAnswer && (
-            <div>
-              <h3>{data.answer}</h3>
-              <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'left',
-                    alignItems: 'start'
-                  }}
-                >
-                  {data.tablature.map((line, idx) => (
-                    <div key={`string-#${idx + 1}`}>{line}</div>
-                  ))}
-                </div>
+      <div style={{ minHeight: '250px' }}>
+        {showAnswer && (
+          <div>
+            <h3>{data.answer}</h3>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'left',
+                  alignItems: 'start'
+                }}
+              >
+                {data.tablature.map((line, idx) => (
+                  <div key={`string-#${idx + 1}`}>{line}</div>
+                ))}
               </div>
             </div>
-          )}
-        </div>
-        <div>
-          <div style={{ marginLeft: 2 }}>
-            {notes[0].map((_, i) => (
-              <span style={{ padding: 3 }}>{i}</span>
-            ))}
           </div>
-          {notes.map((string, idx) => (
-            <div>
-              {`${standardTuning[idx].keyName} `}
-              {string.map((note) => (
-                <span style={{ padding: 2 }}>
-                  <input type="checkbox" disabled checked={note} />
-                </span>
-              ))}
-            </div>
+        )}
+      </div>
+      <div>
+        <div style={{ marginLeft: 2 }}>
+          {notes[0].map((_, i) => (
+            <span style={{ padding: 3 }}>{i}</span>
           ))}
         </div>
-      </>)
-}
+        {notes.map((string, idx) => (
+          <div>
+            {`${standardTuning[idx].keyName} `}
+            {string.map((note) => (
+              <span style={{ padding: 2 }}>
+                <input type="checkbox" disabled checked={note} />
+              </span>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default GuitarNoteGuesser
+export default GuitarNoteGuesser;
